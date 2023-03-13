5c985dd4abb292d17d42494f5f3c346ed33d64ab
salman <najarsalman4@gmail.com>
2023-03-13 22:15:45 +0530

    theme

--- src/components/Tasks/Header.js
+++ src/components/Tasks/Header.js
@@ -1,8 +1,8 @@
 import React, { useEffect, useState } from 'react'
 import '../Styles/Header.css';
 
-const Header = ({theme,setTheme}) => {
-   
+const Header = ({ theme, setTheme }) => {
+
     return (
         <header className='shadow-lg bg-light border border-light mt-2 p-3'>
             <div className='logoimg mx-3'>
@@ -10,12 +10,12 @@ const Header = ({theme,setTheme}) => {
                 <span className='logoName'>Taskmate</span>
             </div>
             <div className="themeSelector ">
-                <span className="light bg-white" onClick={() => setTheme("light")}></span>
-                <span className="medium bg-default " onClick={() => setTheme("medium")}></span>
-                <span className="dark bg-dark" onClick={() => setTheme("dark")}></span>
-                <span className="g1" onClick={() => setTheme("g1")}></span>
-                <span className="g2" onClick={() => setTheme("g2")}></span>
-                <span className="g3" onClick={() => setTheme("g3")}></span>
+                <span className={theme === 'light' ? 'light activeTheme' : 'light'} onClick={() => setTheme("light")}></span>
+                <span className={theme === 'medium' ? 'medium activeTheme' : 'medium'} onClick={() => setTheme("medium")}></span>
+                <span className={theme === 'dark' ? 'dark activeTheme' : 'dark'} onClick={() => setTheme("dark")}></span>
+                <span className={theme === 'g1' ? 'g1 activeTheme' : 'g1'} onClick={() => setTheme("g1")}></span>
+                <span className={theme === 'g2' ? 'g2 activeTheme' : 'g2'} onClick={() => setTheme("g2")}></span>
+                <span className={theme === 'g3' ? 'g3 activeTheme' : 'g3'} onClick={() => setTheme("g3")}></span>
             </div>
         </header>
 
